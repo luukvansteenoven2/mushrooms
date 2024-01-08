3f8745b0f87c7a06d98aade42066c0c8362b7497
TristanvanWeeren <tristanvanweerenschool@gmail.com>
2024-01-08 11:51:14 +0100

    deleted files

--- ilikemushrooms.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan  8 10:33:02 2024
-
-@author: luukv
-"""
-
-print("I like mushrooms")
--- tristanlikesmushrooms.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan  8 11:19:04 2024
-
-@author: trist
-"""
-
-print("I am Tristan I like mushrooms too")
